50b8918c4ca0b9172434bf813ab190591cec6857
unknown <paatatepnadze1@gmail.com>
2022-11-08 14:53:48 +0100

    create controllers

---
+++ AuthServ/AuthServWEbApi/Models/Accounts.cs
@@ -0,0 +1,19 @@
+﻿namespace AuthServWEbApi.Models
+{
+    public class Accounts
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public Role[] Roles { get; set; }
+    }
+
+
+    public enum Role
+    {
+        User,
+        Admin,
+    }
+
+
+}
