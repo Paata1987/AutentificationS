47332301557782c7c883171ae35ecdde11d63363
Paata Home <paata.tepnadze@gmail.com>
2022-12-15 14:32:04 +0100

    create controllers

--- server/router/index.js
+++ server/router/index.js
@@ -1,13 +1,14 @@
 const Router = require('express');
-
+const userController = require('../controlllers/user-controller');
+const UserController = require('../controlllers/user-controller');
 const router = new Router();
 
-router.post('/registration');
-router.post('/login');
-router.post('/logout');
-router.get('/activate/:link');
+router.post('/registration', userController.registration);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+router.get('/activate/:link', userController.activate);
 //endpoint who rewriting access token if it not valid
-router.get('/refresh');
-router.get('/users');
+router.get('/refresh', userController.refresh);
+router.get('/users', userController.getUsers);
 
 module.exports = router;
