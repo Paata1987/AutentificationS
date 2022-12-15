e039e20e38c9b011c4ce5f0d585987e161e534b2
Paata Home <paata.tepnadze@gmail.com>
2022-12-15 14:52:37 +0100

    create mail service / hash password

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,11 +1,23 @@
 const UserModel = require('../models/user-model');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const mailService = require('./mail-service');
+
 class UserService {
   async registration(email, password) {
     const candidate = await UserModel.findOne({ email });
     if (candidate) {
       throw new Error(`User with  ${email} allredy exist!`);
     }
-    const user = await UserModel.create({ email, password });
+
+    const hashPassword = await bcrypt.hash(password, 3);
+    const activationLink = uuid.v4();
+    const user = await UserModel.create({
+      email,
+      password: hashPassword,
+      activationLink,
+    });
+    await mailService.sendActivationMail(email, activationLink);
   }
 }
 
