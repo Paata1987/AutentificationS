027a2beb8d5e263f42fc949295160417255186cb
Paata Home <paata.tepnadze@gmail.com>
2022-12-16 16:39:31 +0100

    create seeviser

--- client/my-app/src/services/UserService.ts
+++ client/my-app/src/services/UserService.ts
@@ -1,9 +1,10 @@
 import { AxiosResponse } from 'axios';
 import api from '../http';
 import { AuthResponse } from '../models/AuthResponse';
+import { IUser } from '../models/IUser';
 
 export default class Userervice {
   static fetchUsers() {
-    return api.get('/users');
+    return api.get<IUser>('/users');
   }
 }
