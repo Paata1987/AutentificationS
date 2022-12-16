0c9e13f4d24617123d0f2f50c3d1df64bfba21c0
Paata Home <paata.tepnadze@gmail.com>
2022-12-16 15:13:54 +0100

    create auth and user services

--- client/my-app/src/http/index.ts
+++ client/my-app/src/http/index.ts
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 export const API_URL = 'http://localhost:5000/api';
 
-const $api = axios.create({
+const api = axios.create({
   withCredentials: true,
   baseURL: API_URL,
 });
 
-$api.interceptors.request.use((config: any) => {
+api.interceptors.request.use((config: any) => {
   config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
   return config;
 });
-export default $api;
+export default api;
