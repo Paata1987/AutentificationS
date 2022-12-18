31d897f7e7bb616a8bbdfb0becf68aec69d2f185
Paata Home <paata.tepnadze@gmail.com>
2022-12-18 19:35:35 +0100

    tokent refresh component

--- client/my-app/src/http/index.ts
+++ client/my-app/src/http/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { AuthResponse } from '../models/AuthResponse';
 
 export const API_URL = 'http://localhost:5000/api';
 
@@ -11,4 +12,32 @@ api.interceptors.request.use((config: any) => {
   config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
   return config;
 });
+
+api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    const renewRequest = error.config;
+    if (
+      error.response.status == 401 &&
+      error.config &&
+      !error.config._isRetry
+    ) {
+      renewRequest._isRetry = true;
+      try {
+        const response = await axios.get<AuthResponse>(`${API_URL}/refreah`, {
+          withCredentials: true,
+        });
+        localStorage.setItem('token', response.data.accessToken);
+        return api.request(renewRequest);
+      } catch (error) {
+        console.log(' notAuthorized user');
+      }
+    }
+
+    throw error;
+  }
+);
+
 export default api;
